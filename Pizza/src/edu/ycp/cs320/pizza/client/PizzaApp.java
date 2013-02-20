013d0cacc7f363cc449e9293fc90fc9430dd22e6
Joey Maloney <jmalone4@ycp.edu>
2013-02-20 09:51:20 -0500

    jjjj

--- Pizza/src/edu/ycp/cs320/pizza/client/PizzaApp.java
+++ Pizza/src/edu/ycp/cs320/pizza/client/PizzaApp.java
@@ -2,9 +2,13 @@ package edu.ycp.cs320.pizza.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootLayoutPanel;
+
+import edu.ycp.cs320.pizza.shared.Pizza;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -16,9 +20,26 @@ public class PizzaApp implements EntryPoint {
 	public void onModuleLoad() {
 		FlowPanel panel = new FlowPanel();
 		
-		Label helloWorld = new Label("Hello, world!");
+		//Label helloWorld = new Label("Hello, world!");
 		
-		panel.add(helloWorld);
+		//panel.add(helloWorld);
+		
+		PizzaView pizzaView=new PizzaView();
+		panel.add(pizzaView);
+		
+		Pizza model=new Pizza();
+		
+		pizzaView.setModel(model);
+		pizzaView.update();
+		
+		
+		Button b=new Button("PRESS");
+		b.addClickHandler(new ClickHandler(){
+			
+			@Override
+			publicvoid onClick(ClickEvent event)
+			
+		}
 		
 		RootLayoutPanel.get().add(panel);
 		RootLayoutPanel.get().setWidgetTopBottom(panel, 10.0, Unit.PX, 10.0, Unit.PX);
--- Pizza/src/edu/ycp/cs320/pizza/client/PizzaView.java
+++ Pizza/src/edu/ycp/cs320/pizza/client/PizzaView.java
@@ -1,12 +1,23 @@
 package edu.ycp.cs320.pizza.client;
 
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.InlineLabel;
 import com.google.gwt.user.client.ui.LayoutPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.user.client.ui.ListBox;
 
+import edu.ycp.cs320.pizza.shared.Pizza;
+import edu.ycp.cs320.pizza.shared.Size;
+
 public class PizzaView extends Composite {
+	private Pizza model;
+	private InlineLabel sizeLabel;
+	private InlineLabel toppingLabel;
+	private ListBox sizeComboBox;
+	
+	
+	
 	public PizzaView() {
 		
 		LayoutPanel layoutPanel = new LayoutPanel();
@@ -22,9 +33,24 @@ public class PizzaView extends Composite {
 		layoutPanel.setWidgetLeftWidth(toppingLabel, 29.0, Unit.PX, 56.0, Unit.PX);
 		layoutPanel.setWidgetTopHeight(toppingLabel, 94.0, Unit.PX, 18.0, Unit.PX);
 		
-		ListBox comboBox = new ListBox();
-		layoutPanel.add(comboBox);
-		layoutPanel.setWidgetLeftWidth(comboBox, 169.0, Unit.PX, 65.0, Unit.PX);
-		layoutPanel.setWidgetTopHeight(comboBox, 25.0, Unit.PX, 26.0, Unit.PX);
+		sizeComboBox = new ListBox();
+		layoutPanel.add(sizeComboBox);
+		layoutPanel.setWidgetLeftWidth(sizeComboBox, 169.0, Unit.PX, 65.0, Unit.PX);
+		layoutPanel.setWidgetTopHeight(sizeComboBox, 25.0, Unit.PX, 26.0, Unit.PX);
 	}
+	public void setModel(Pizza model){
+		this.model=model;
+	}
+	public void update(){
+		if(sizeComboBox.getItemCount()==0){
+			Size[] sizes=Size.values();
+			for(Size s:sizes){
+				sizeComboBox.addItem(s.toString());
+			}
+		}
+		//set pizza's current size in the combo box
+		Size pizzaSize=model.getSize();
+		sizeComboBox.setSelectedIndex(pizzaSize.ordinal());
+	}
+	
 }
